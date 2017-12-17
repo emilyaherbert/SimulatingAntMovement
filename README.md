b547c07af7b99311157741d58032e3e5fa814961
eherbert <eherbert@trinity.edu>
2017-12-16 19:22:37 -0600

    working on README

--- README.md
+++ README.md
@@ -6,4 +6,14 @@ In order to find the ant locations in each frame, I clustered on points of certa
 
 There is a known Same Size K Means algorithm that works to combat this already, but this would not work for me. The 20 ants each have different amount of pixels that they consume in each photo and searching for clusters of the same size wouldn't produce the results that I wanted. This algorithm isn't native to Scala and I didn't end up implementing it, so I am not able to compare it in application.
 
-This led me to developing a [Similar Size K Means](https://github.com/eherbert/SimulatingAntMovement/blob/master/src/main/scala/utility/SimilarSizeKMeans.scala) clustering algorithm.
+This led me to developing a [Similar Size K Means](https://github.com/eherbert/SimulatingAntMovement/blob/master/src/main/scala/utility/SimilarSizeKMeans.scala) clustering algorithm. It works by running a normal K Means and then checking to see if the clusters are evenly weighted. If they are, it returns the cluster centers. If they are not, it moves the cluster centers from the smallest sized clusters to random points from the largest sized clusters, effectively splitting them. You pass the algorithm a stdDevBreakpoint and a stdDevTolerance where the clusters are deemed evenly weighted if the standard deviation is below the stdDevBreakpoint and where, if they are not evenly weighted, the clusters larger than the standard deviation * stdDevTolerance are targeted for splitting. Normal K Means is run again and this process repeats until the converged cluster centers from the K Means algorithm are evenly weighted.
+
+Here are the results of K Means compared to results of Similar Size K Means.
+
+K Means:
+
+![alt text](images/kmeans.png "K Means Results")
+
+Similar Size K Means:
+
+![alt text](images/similarsizekmeans.png "Similar Size K Means Results")
--- src/main/scala/utility/SimilarSizeKMeans.scala
+++ src/main/scala/utility/SimilarSizeKMeans.scala
@@ -7,30 +7,25 @@ import org.apache.spark.sql.functions._
 class SimilarSizeKMeans(private val numClusters: Int,
     private val featuresCol: String,
     private val predictionCol: String,
-    private val stdDevBreakpoint:Double,
+    private val stdDevBreakpoint: Double,
     private val stdDevTolerance: Double) extends Serializable {
 
   def findClusterCenters(data: Dataset[Row]): Array[(Double, Double)] = {
     var startingClusterCenters = data
       .orderBy(rand())
       .take(20)
-      .map{r =>
+      .map { r =>
         r.getDouble(0) -> r.getDouble(1)
       }.zipWithIndex
 
     // wow i just really hate everything about this
-    var (clusterCenters, predictions) = runClustering(data, startingClusterCenters, Array[((Double,Double),Int)](), 0, data)
-    println("1num clusters: " + clusterCenters.length)
+    var (clusterCenters, predictions) = runClustering(data, startingClusterCenters, Array[((Double, Double), Int)](), 0, data)
     var (isEvenlyWeightedRes, clusterSizes, averageClusterSize, stdDev) = isEvenlyWeighted(predictions)
-    println("2num clusters: " + clusterCenters.length)
     predictions.show()
-    var iterations = 0
-    while (!isEvenlyWeightedRes && iterations < 10) {
+    while (!isEvenlyWeightedRes) {
       clusterCenters = splitClusters(predictions, clusterSizes, averageClusterSize, stdDev, clusterCenters)
-      println("3num clusters: " + clusterCenters.length)
-      val tmp = runClustering(data, clusterCenters, Array[((Double,Double), Int)](), 0, data)
+      val tmp = runClustering(data, clusterCenters, Array[((Double, Double), Int)](), 0, data)
       clusterCenters = tmp._1
-      println("4num clusters: " + clusterCenters.length)
       predictions = tmp._2
       val tmp2 = isEvenlyWeighted(predictions)
       isEvenlyWeightedRes = tmp2._1
@@ -43,11 +38,11 @@ class SimilarSizeKMeans(private val numClusters: Int,
     clusterCenters.map(_._1)
   }
 
-  def runClustering(data: Dataset[Row],
-    centers: Array[((Double,Double), Int)],
-    oldCenters: Array[((Double,Double), Int)],
+  private def runClustering(data: Dataset[Row],
+    centers: Array[((Double, Double), Int)],
+    oldCenters: Array[((Double, Double), Int)],
     iteration: Int,
-    clusters: Dataset[Row]): (Array[((Double,Double), Int)], Dataset[Row]) = {
+    clusters: Dataset[Row]): (Array[((Double, Double), Int)], Dataset[Row]) = {
 
     def nearestClusterFn(x: Double, y: Double): Int = {
       centers.map { r =>
@@ -70,7 +65,7 @@ class SimilarSizeKMeans(private val numClusters: Int,
         .avg("x", "y")
         .select(col("avg(x)").as("x"), col("avg(y)").as("y"))
         .collect()
-        .map{r =>
+        .map { r =>
           (r.getDouble(0) -> r.getDouble(1))
         }.zipWithIndex
 
@@ -78,7 +73,7 @@ class SimilarSizeKMeans(private val numClusters: Int,
     }
   }
 
-  def isEvenlyWeighted(predictions: Dataset[Row]): (Boolean, Array[(Int,Int)], Double, Double) = {
+  private def isEvenlyWeighted(predictions: Dataset[Row]): (Boolean, Array[(Int, Int)], Double, Double) = {
     val clusterSizes = predictions
       .groupBy("cluster")
       .count()
@@ -96,36 +91,32 @@ class SimilarSizeKMeans(private val numClusters: Int,
       .select(sum("dist"))
       .head()
       .getDouble(0) / predictions.count()
-      
-    println("stdDev: " + stdDev)
-    
+
     (stdDev < stdDevBreakpoint, clusterSizes.collect().map(r => (r.getInt(0) -> r.getLong(1).toInt)), averageClusterSize, stdDev)
   }
 
-  def splitClusters(predictions: Dataset[Row],
-    clusterSizes: Array[(Int,Int)],
+  private def splitClusters(predictions: Dataset[Row],
+    clusterSizes: Array[(Int, Int)],
     averageClusterSize: Double,
     stdDev: Double,
-    clusterCenters:Array[((Double,Double),Int)]): Array[((Double,Double), Int)] = {
+    clusterCenters: Array[((Double, Double), Int)]): Array[((Double, Double), Int)] = {
 
     val above = clusterSizes.filter(r => r._2 > (averageClusterSize + stdDev * stdDevTolerance)).sortBy(-_._2)
     val below = clusterSizes.filter(r => r._2 < (averageClusterSize - stdDev * stdDevTolerance)).sortBy(_._2)
 
-    above.foreach(println)
-    
     var rover = 0
-    for(cluster <- above.map(_._1)) {
+    for (cluster <- above.map(_._1)) {
       val randomLoc = predictions
-        .filter(col("cluster")===cluster.toString())
+        .filter(col("cluster") === cluster.toString())
         .orderBy(rand())
         .take(1)
-        .map{r =>
+        .map { r =>
           (r.getDouble(0) -> r.getDouble(1))
         }.head
       clusterCenters(rover) = randomLoc -> clusterCenters(rover)._2
-      rover+=1
+      rover += 1
     }
-    
+
     clusterCenters
   }
 }
